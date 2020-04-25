fe66fb2f290a71aa17c1228c819b13810f195258
ashish007kumar <ashishkumar357ak@gmail.com>
2020-04-25 19:12:14 +0530

    jump

---
+++ Jump Array Game.cpp
@@ -0,0 +1,14 @@
+int Solution::canJump(vector<int> &A) {
+    int end=A.size()-1;
+    for(int i=A.size()-2;i>=0;i--){
+        if(A[i]+i >=end ){
+            end=i;
+        }
+    }
+    if(end==0){
+        return 1;
+    }
+    else{
+        return 0;
+    }
+}
