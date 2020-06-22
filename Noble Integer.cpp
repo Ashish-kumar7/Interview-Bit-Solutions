d96126007694dca43fa3e56fd5cb9de79849039e
ashish007kumar <ashishkumar357ak@gmail.com>
2020-06-22 21:02:59 +0530

    noble

---
+++ Noble Integer.cpp
@@ -0,0 +1,13 @@
+int Solution::solve(vector <int>&A) {
+    
+    sort(A.begin(), A.end());
+    
+    for(int i=0;i<A.size();i++){
+        if(A[i]==A[i+1] && i!=A.size()-1){
+        continue;
+    }
+        if(A[i]==A.size()-1-i)
+        return 1;
+    }
+        return -1;
+}
